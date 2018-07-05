08528c85a8e7a6627a5b513db60d435f125c4a9a
Hai Dinh <ngochaidinh94@gmail.com>
2018-07-05 13:23:24 +0800

    Sanitize text to avoid new line

--- scrape.py
+++ scrape.py
@@ -4,6 +4,9 @@ from bs4 import BeautifulSoup
 URL = 'https://en.wikipedia.org/wiki/Mobile_country_code'
 OUT = 'mcc-mnc.tsv'
 
+def sanitize(text):
+    return ' '.join(text.strip().replace('"', '').split())
+
 def main():
     soup = BeautifulSoup(requests.get(URL).content, 'html.parser')
     tables = soup.find_all('table', attrs={'class':'wikitable'})
@@ -12,7 +15,7 @@ def main():
         [f.write(u'{0}{1}\t{2}\t{3}\n'.format(*cols[:4]).encode('utf8'))
             for table in tables
             for row in table.find_all('tr')
-            for cols in [[col.text.strip().replace('"', '') for col in row.find_all('td')]]
+            for cols in [[sanitize(col.text) for col in row.find_all('td')]]
             if len(cols) > 3 and cols[2] != 'TEST'
         ]
 
