a69ea91ee41da3be32ac621374c46d9c9ce9e394
Hai Dinh <ngochaidinh94@gmail.com>
2018-07-05 13:19:06 +0800

    Remove illegal quoting in output file

--- scrape.py
+++ scrape.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 URL = 'https://en.wikipedia.org/wiki/Mobile_country_code'
-OUT = 'out.tsv'
+OUT = 'mcc-mnc.tsv'
 
 def main():
     soup = BeautifulSoup(requests.get(URL).content, 'html.parser')
@@ -12,7 +12,7 @@ def main():
         [f.write(u'{0}{1}\t{2}\t{3}\n'.format(*cols[:4]).encode('utf8'))
             for table in tables
             for row in table.find_all('tr')
-            for cols in [[col.text.strip() for col in row.find_all('td')]]
+            for cols in [[col.text.strip().replace('"', '') for col in row.find_all('td')]]
             if len(cols) > 3 and cols[2] != 'TEST'
         ]
 
